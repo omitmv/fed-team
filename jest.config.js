11cdd0cc20d8ac3a7662604fb961e7ab96e6ba65
Rafael Valverde <omit.mv@gmail.com>
2025-08-01 09:54:25 -0300

    Refatoração das páginas

--- jest.config.js
+++ jest.config.js
@@ -3,23 +3,21 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(axios)/)'
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
     '!src/index.tsx',
-    '!src/reportWebVitals.ts',
+    '!src/reportWebVitals.ts'
   ],
   coverageThreshold: {
     global: {
       branches: 50,
       functions: 50,
       lines: 50,
-      statements: 50,
-    },
-  },
-};
+      statements: 50
+    }
+  }
+}
